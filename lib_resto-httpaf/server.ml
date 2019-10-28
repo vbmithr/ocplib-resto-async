9ae71e7d0884d4a07a87f5cf991d96bb311cdc7a
Vincent Bernardoff <vb@luminar.eu.org>
2019-10-28 17:24:15 +0800

    wip

--- lib_resto-httpaf/server.ml
+++ lib_resto-httpaf/server.ml
@@ -23,15 +23,13 @@
 (*                                                                           *)
 (*****************************************************************************)
 
+open Core
 open Async
 
 let pp_print_addr ppf a =
   Format.fprintf ppf "%s" (Socket.Address.to_string a)
 
-module AddressMap = Map.Make(struct
-    include Socket.Address.Blocking_sexp
-    let compare = Stdlib.compare
-  end)
+module AddressMap = Map.Make(Socket.Address.Inet.Blocking_sexp)
 
 module Make (Encoding : Resto.ENCODING) (Log : Logs_async.LOG) = struct
 
@@ -52,10 +50,152 @@ module Make (Encoding : Resto.ENCODING) (Log : Logs_async.LOG) = struct
 
   let create_pipe server addr to_string s =
     let stream = Pipe.map s ~f:to_string in
-    server.streams <- AddressMap.add addr stream server.streams ;
+    server.streams <- AddressMap.set ~key:addr ~data:stream server.streams ;
     stream
 
-  let callback server addr { Request.meth; target; headers; _ } body =
+  let get_input_media_type server { Request.headers; _ } =
+    match Headers.get headers "content-type" with
+    | None -> Deferred.Result.return (snd server.default_media_type)
+    | Some content_type ->
+        match Utils.split_path content_type with
+        | [x ; y] -> begin
+            match Media_type.find_media (x, y) server.media_types with
+            | None ->
+                return (Error (`Unsupported_media_type content_type))
+            | Some media_type ->
+                Deferred.Result.return media_type
+          end
+        | _ ->
+            return (Error (`Unsupported_media_type content_type))
+
+  let get_accept server { Request.headers; _ } =
+    match Headers.get headers "accept" with
+    | None -> Deferred.Result.return server.default_media_type
+    | Some accepted ->
+        match Media_type.resolve_accept_header
+                server.media_types (Some accepted) with
+        | None -> return (Error `Not_acceptable)
+        | Some media_type -> Deferred.Result.return media_type
+
+  let handle_most ({ Request.meth; target; headers; _ } as req) path server addr =
+    Directory.lookup server.root () meth path >>=? fun (Directory.Service s) ->
+    get_input_media_type server req >>=? fun input_media_type ->
+    Log.debug (fun m -> m "(%a) input media type %s" pp_print_addr
+                  addr (Media_type.name input_media_type)) >>= fun () ->
+    get_accept server req >>=? fun (output_content_type, output_media_type) ->
+    let query = Uri.(query (of_string target)) in
+    begin match Resto.Query.parse s.types.query
+                  (List.Assoc.map query ~f:(String.concat ~sep:",")) with
+    | exception (Resto.Query.Invalid s) -> return (Error (`Cannot_parse_query s))
+    | query -> Deferred.Result.return query
+    end >>=? fun query ->
+    Log.debug (fun m -> m "(%a) ouput media type %s" pp_print_addr
+                  addr (Media_type.name output_media_type)) >>= fun () ->
+    let output = output_media_type.construct s.types.output
+    and error = function
+    | None -> assert false
+    | Some e ->
+        let s = output_media_type.construct s.types.error e in
+        Cohttp_async.Body.of_string s,
+        Transfer.Fixed (Int64.of_int (String.length s)) in
+    let new_headers =
+      Headers.(add empty "content-type" output_content_type) in
+    let new_headers = Cors.add_allow_origin
+        headers server.cors (Headers.get headers "origin") in
+    begin
+      match s.types.input with
+      | Service.No_input -> Deferred.ok (s.handler query ())
+      | Service.Input input ->
+          Utils.read_body body >>= fun body ->
+          match input_media_type.destruct input body with
+          | Error s -> return (Error (`Cannot_parse_body s))
+          | Ok body -> Deferred.ok (s.handler query body)
+    end >>=? function
+    | `Ok o ->
+        let body = output o in
+        let encoding =
+          Transfer.Fixed (Int64.of_int (String.length body)) in
+        Deferred.Result.return
+          (Response.make ~status:`OK ~encoding ~headers (),
+           Cohttp_async.Body.of_string body)
+    | `OkPipe o ->
+        let body = create_pipe server addr output o in
+        let encoding = Transfer.Chunked in
+        return (Ok
+                  (Response.make ~status:`OK ~encoding ~headers (),
+                   Cohttp_async.Body.of_pipe body))
+    | `Created s ->
+        let headers = Header.init () in
+        let headers =
+          match s with
+          | None -> headers
+          | Some s -> Header.add headers "location" s in
+        Deferred.Result.return
+          (Response.make ~status:`Created  ~headers (),
+           Cohttp_async.Body.empty)
+    | `No_content ->
+        Deferred.Result.return
+          (Response.make ~status:`No_content (),
+           Cohttp_async.Body.empty)
+    | `Unauthorized e ->
+        let body, encoding = error e in
+        let status = `Unauthorized in
+        Deferred.Result.return
+          (Response.make ~status ~encoding ~headers (), body)
+    | `Forbidden e ->
+        let body, encoding = error e in
+        let status = `Forbidden in
+        Deferred.Result.return
+          (Response.make ~status ~encoding ~headers (), body)
+    | `Not_found e ->
+        let body, encoding = error e in
+        let status = `Not_found in
+        Deferred.Result.return
+          (Response.make ~status ~encoding ~headers (), body)
+    | `Conflict e ->
+        let body, encoding = error e in
+        let status = `Conflict in
+        Deferred.Result.return
+          (Response.make ~status ~encoding ~headers (), body)
+    | `Error e ->
+        let body, encoding = error e in
+        let status = `Internal_server_error in
+        Deferred.Result.return
+          (Response.make ~status ~encoding ~headers (), body)
+
+  let handle_options server { Request.headers; _ } path =
+    let origin_header = Headers.get headers "origin" in
+    begin
+      (* Default OPTIONS handler for CORS preflight *)
+      if origin_header = None then
+        Directory.allowed_methods server.root () path
+      else
+        match Headers.get headers
+                "Access-Control-Request-Method" with
+        | None ->
+            Directory.allowed_methods server.root () path
+        | Some meth ->
+            let meth = Method.of_string meth in
+            Directory.lookup server.root () meth path >>=? fun _handler ->
+            return (Ok [ meth ])
+        | _ ->
+            return (Error `Not_found)
+    end >>=? fun cors_allowed_meths ->
+    Log.info begin fun m ->
+      m "(%a) RPC preflight" pp_print_addr addr
+    end >>= fun () ->
+    let headers = Header.init () in
+    let headers =
+      Header.add_multi headers
+        "Access-Control-Allow-Methods"
+        (List.map Resto.string_of_meth cors_allowed_meths) in
+    let headers = Cors.add_headers headers server.cors origin_header in
+    return (Ok
+              (Response.make ~flush:true ~status:`OK ~headers (),
+               Cohttp_async.Body.empty))
+
+  let request_handler addr reqd =
+    let req = Reqd.request reqd in
     (* FIXME: check inbound adress *)
     let uri = Uri.of_string target in
     let path = Uri.pct_decode (Uri.path uri) in
@@ -63,168 +203,11 @@ module Make (Encoding : Resto.ENCODING) (Log : Logs_async.LOG) = struct
       m "(%a) receive request to %s" pp_print_addr addr path
     end >>= fun () ->
     let path = Utils.split_path path in
-    begin
-      match meth with
-      | `DELETE | `GET | `POST | `PUT | `Other "PATCH" as meth -> begin
-          let meth = match meth with
-            | `Other "PATCH" -> `PATCH
-            | `Other _ -> assert false
-            | #Resto.meth as m -> m in
-          Directory.lookup server.root ()
-            meth path >>=? fun (Directory.Service s) ->
-          begin
-            match Headers.get headers "content-type" with
-            | None -> Deferred.Result.return (snd server.default_media_type)
-            | Some content_type ->
-                match Utils.split_path content_type with
-                | [x ; y] -> begin
-                    match Media_type.find_media (x, y) server.media_types with
-                    | None ->
-                        return (Error (`Unsupported_media_type content_type))
-                    | Some media_type ->
-                        Deferred.Result.return media_type
-                  end
-                | _ ->
-                    return (Error (`Unsupported_media_type content_type))
-          end >>=? fun input_media_type ->
-          Log.debug begin fun m ->
-            m "(%a) input media type %s"
-              pp_print_addr addr
-              (Media_type.name input_media_type)
-          end >>= fun () ->
-          begin
-            match Headers.get headers "accept" with
-            | None -> Deferred.Result.return server.default_media_type
-            | Some accepted ->
-                match Media_type.resolve_accept_header
-                        server.media_types (Some accepted) with
-                | None -> return (Error `Not_acceptable)
-                | Some media_type -> Deferred.Result.return media_type
-          end >>=? fun (output_content_type, output_media_type) ->
-          begin
-            match Resto.Query.parse s.types.query
-                    (List.map
-                       (fun (k, l) -> (k, String.concat "," l))
-                       (Uri.query uri)) with
-            | exception (Resto.Query.Invalid s) ->
-                return (Error (`Cannot_parse_query s))
-            | query -> Deferred.Result.return query
-          end >>=? fun query ->
-          Log.debug begin fun m ->
-            m "(%a) ouput media type %s"
-              pp_print_addr addr
-              (Media_type.name output_media_type)
-          end >>= fun () ->
-          let output = output_media_type.construct s.types.output
-          and error = function
-            | None -> Cohttp_async.Body.empty, Transfer.Fixed 0L
-            | Some e ->
-                let s = output_media_type.construct s.types.error e in
-                Cohttp_async.Body.of_string s,
-                Transfer.Fixed (Int64.of_int (String.length s)) in
-          let headers = Header.init () in
-          let headers =
-            Header.add headers "content-type" output_content_type in
-          let headers = Cors.add_allow_origin
-              headers server.cors (Header.get req_headers "origin") in
-          begin
-            match s.types.input with
-            | Service.No_input -> Deferred.ok (s.handler query ())
-            | Service.Input input ->
-                Cohttp_async.Body.to_string body >>= fun body ->
-                match input_media_type.destruct input body with
-                | Error s -> return (Error (`Cannot_parse_body s))
-                | Ok body -> Deferred.ok (s.handler query body)
-          end >>=? function
-          | `Ok o ->
-              let body = output o in
-              let encoding =
-                Transfer.Fixed (Int64.of_int (String.length body)) in
-              Deferred.Result.return
-                (Response.make ~status:`OK ~encoding ~headers (),
-                 Cohttp_async.Body.of_string body)
-          | `OkPipe o ->
-              let body = create_pipe server addr output o in
-              let encoding = Transfer.Chunked in
-              return (Ok
-                        (Response.make ~status:`OK ~encoding ~headers (),
-                         Cohttp_async.Body.of_pipe body))
-          | `Created s ->
-              let headers = Header.init () in
-              let headers =
-                match s with
-                | None -> headers
-                | Some s -> Header.add headers "location" s in
-              Deferred.Result.return
-                (Response.make ~status:`Created  ~headers (),
-                 Cohttp_async.Body.empty)
-          | `No_content ->
-              Deferred.Result.return
-                (Response.make ~status:`No_content (),
-                 Cohttp_async.Body.empty)
-          | `Unauthorized e ->
-              let body, encoding = error e in
-              let status = `Unauthorized in
-              Deferred.Result.return
-                (Response.make ~status ~encoding ~headers (), body)
-          | `Forbidden e ->
-              let body, encoding = error e in
-              let status = `Forbidden in
-              Deferred.Result.return
-                (Response.make ~status ~encoding ~headers (), body)
-          | `Not_found e ->
-              let body, encoding = error e in
-              let status = `Not_found in
-              Deferred.Result.return
-                (Response.make ~status ~encoding ~headers (), body)
-          | `Conflict e ->
-              let body, encoding = error e in
-              let status = `Conflict in
-              Deferred.Result.return
-                (Response.make ~status ~encoding ~headers (), body)
-          | `Error e ->
-              let body, encoding = error e in
-              let status = `Internal_server_error in
-              Deferred.Result.return
-                (Response.make ~status ~encoding ~headers (), body)
-        end
-      | `HEAD ->
-          (* TODO ??? *)
-          return (Error `Not_implemented)
-      | `OPTIONS ->
-          let req_headers = Request.headers req in
-          let origin_header = Header.get req_headers "origin" in
-          begin
-            (* Default OPTIONS handler for CORS preflight *)
-            if origin_header = None then
-              Directory.allowed_methods server.root () path
-            else
-              match Header.get req_headers
-                      "Access-Control-Request-Method" with
-              | None ->
-                  Directory.allowed_methods server.root () path
-              | Some meth ->
-                  match Code.method_of_string meth with
-                  | #Resto.meth as meth ->
-                      Directory.lookup server.root () meth path >>=? fun _handler ->
-                      return (Ok [ meth ])
-                  | _ ->
-                      return (Error `Not_found)
-          end >>=? fun cors_allowed_meths ->
-          Log.info begin fun m ->
-            m "(%a) RPC preflight" pp_print_addr addr
-          end >>= fun () ->
-          let headers = Header.init () in
-          let headers =
-            Header.add_multi headers
-              "Access-Control-Allow-Methods"
-              (List.map Resto.string_of_meth cors_allowed_meths) in
-          let headers = Cors.add_headers headers server.cors origin_header in
-          return (Ok
-                    (Response.make ~flush:true ~status:`OK ~headers (),
-                     Cohttp_async.Body.empty))
-      | _ ->
-          return (Error `Not_implemented)
+    begin match req.Request.meth with
+      | `DELETE | `GET | `POST | `PUT | `Other "PATCH" -> handle_most path server
+      | `OPTIONS -> handle_options path
+      | `HEAD -> return (Error `Not_implemented)
+      | _ -> return (Error `Not_implemented)
     end >>= function
     | Ok answer -> return answer
     | Error `Not_implemented ->
@@ -309,31 +292,30 @@ module Make (Encoding : Resto.ENCODING) (Log : Logs_async.LOG) = struct
         Base.Option.iter ~f:Pipe.close_read
           (AddressMap.find_opt addr t.streams)
       end
-    (* and on_exn = function
-     *   | Unix.Unix_error (Unix.EADDRINUSE, "bind", _) ->
-     *       log_error "RPC server port already taken, \
-     *                  the node will be shutdown" ;
-     *       exit 1
-     *   | Unix.Unix_error (ECONNRESET, _, _)
-     *   | Unix.Unix_error (EPIPE, _, _)  -> ()
-     *   | exn -> !Lwt.async_exception_hook exn
-     * and callback ~body socket req =
-     *   Monitor.try_with ~extract_exn:true
-     *     (fun () -> callback t socket req body) >>= function
-     *   | Ok r -> return r
-     *   | Error Not_found ->
-     *       let status = `Not_found in
-     *       let body = Cohttp_async.Body.empty in
-     *       return (Response.make ~status (), body)
-     *   | Error exn ->
-     *       let headers = Header.init () in
-     *       let headers =
-     *         Header.add headers "content-type" "text/ocaml.exception" in
-     *       let status = `Internal_server_error in
-     *       let body = Cohttp_async.Body.of_string (Printexc.to_string exn) in
-     *       return (Response.make ~status ~headers (), body) *)
+      (* and on_exn = function
+       *   | Unix.Unix_error (Unix.EADDRINUSE, "bind", _) ->
+       *       log_error "RPC server port already taken, \
+       *                  the node will be shutdown" ;
+       *       exit 1
+       *   | Unix.Unix_error (ECONNRESET, _, _)
+       *   | Unix.Unix_error (EPIPE, _, _)  -> ()
+       *   | exn -> !Lwt.async_exception_hook exn
+       * and callback ~body socket req =
+       *   Monitor.try_with ~extract_exn:true
+       *     (fun () -> callback t socket req body) >>= function
+       *   | Ok r -> return r
+       *   | Error Not_found ->
+       *       let status = `Not_found in
+       *       let body = Cohttp_async.Body.empty in
+       *       return (Response.make ~status (), body)
+       *   | Error exn ->
+       *       let headers = Header.init () in
+       *       let headers =
+       *         Header.add headers "content-type" "text/ocaml.exception" in
+       *       let status = `Internal_server_error in
+       *       let body = Cohttp_async.Body.of_string (Printexc.to_string exn) in
+       *       return (Response.make ~status ~headers (), body) *)
     in
-    let request_handler _ _ = () in
     let error_handler _ ?request:_ _ _ = () in
     Tcp.Server.create_sock
       ?max_connections ?max_accepts_per_batch ?backlog ?socket
--- lib_resto-httpaf/utils.ml
+++ lib_resto-httpaf/utils.ml
@@ -24,25 +24,9 @@
 (*****************************************************************************)
 
 let split_path path =
-  let l = String.length path in
-  let rec do_slashes acc i =
-    if i >= l then
-      List.rev acc
-    else if String.get path i = '/' then
-      do_slashes acc (i + 1)
-    else
-      do_component acc i i
-  and do_component acc i j =
-    if j >= l then
-      if i = j then
-        List.rev acc
-      else
-        List.rev (String.sub path i (j - i) :: acc)
-    else if String.get path j = '/' then
-      do_slashes (String.sub path i (j - i) :: acc) j
-    else
-      do_component acc i (j + 1) in
-  do_slashes [] 0
+  List.filter_map
+    (function "" -> None | s -> Some s)
+    (String.split_on_char '/' path)
 
 open Core
 open Async
